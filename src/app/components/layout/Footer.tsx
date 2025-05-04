99d416a75f7c21f095e1e66fd14094c7d14f6a26
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2025-05-04 21:01:09 +0530

    add mobile footer padding

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -11,7 +11,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white ">
-      <div className="container mx-auto px-4 py-12">
+      <div className="container mx-auto px-6 md:px-4 py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Company Info */}
           <div>
